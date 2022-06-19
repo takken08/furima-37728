e05eccb863be7df93d268070495953a83890bfdc
takken08 <yoshitake130814@gmail.com>
2022-06-19 17:01:33 +0900

    素材ファイルをfurimaのディレクトリに配置した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -7,5 +9,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'take' && password == '0814'
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_katakana, :first_name_katakana, :date_of_birth])
   end
 end
