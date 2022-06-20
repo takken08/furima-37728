1c98088fb4ac1668fd21e4b0785b0eef12330e46
takken08 <yoshitake130814@gmail.com>
2022-06-20 20:26:21 +0900

    新たにバリデーションを記述した

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,            presence: true
-  validates :last_name,           presence: true
-  validates :first_name,          presence: true
-  validates :last_name_katakana,  presence: true
-  validates :first_name_katakana, presence: true
+
+  Valid_name_zenkaku = /\A[ぁ-んァ-ン一-龥]/
+  Valid_name_kana = /\A[ァ-ヶー－]+\z/
+  with_options presence: true do
+    validates :last_name,           format: { with: Valid_name_zenkaku, message: 'Please enter in Full-width characters.' }
+    validates :first_name,          format: { with: Valid_name_zenkaku, message: 'Please enter in Full-width characters.' }
+    validates :last_name_katakana,  format: { with: Valid_name_kana, message: 'Please enter in katakana characters' }
+    validates :first_name_katakana, format: { with: Valid_name_kana, message: 'Please enter in katakana characters' }
+  end
   validates :date_of_birth,       presence: true
 end
