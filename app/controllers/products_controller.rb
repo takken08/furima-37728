b078bfb6b9f189eae4934eb43c432f812383878c
takken08 <yoshitake130814@gmail.com>
2022-06-24 17:28:28 +0900

    アクション定義とストロングパラメーターを記述した

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,17 @@
 class ProductsController < ApplicationController
 
   def index
-  
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new(product_params)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:item_name, :explanation, :category_id, :item_state_id, :delivery_charge_id, :area_id, :delivery_day_id, :selling_price)
   end
 
 end
