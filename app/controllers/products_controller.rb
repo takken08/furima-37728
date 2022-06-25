67a4d773498d7d1a0b156fb59b8b26dd768b59ba
takken08 <yoshitake130814@gmail.com>
2022-06-25 20:59:17 +0900

    画像の保存を許可するストロングパラメーター

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,7 @@ class ProductsController < ApplicationController
 
   private
 
+  def product_params
+    params.require(:product).permit(:item_name, :explanation, :category_id, :item_state_id, :delivery_charge_id, :area_id, :delivery_day_id, :selling_price, :image).merge(user_id: current_user.id)
+  end
 end
