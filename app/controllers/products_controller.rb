2d23bdfda775527671587c2bd1942ecc6ecaac2b
takken08 <yoshitake130814@gmail.com>
2022-06-25 20:17:56 +0900

    フォームのhogeを書き換えた

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,13 +5,9 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @product = Product.new(product_params)
+    @product = Product.new
   end
 
   private
-
-  def product_params
-    params.require(:product).permit(:item_name, :explanation, :category_id, :item_state_id, :delivery_charge_id, :area_id, :delivery_day_id, :selling_price)
-  end
 
 end
