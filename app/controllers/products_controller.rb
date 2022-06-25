bde0cad5f63642efc4263db190ca15f7a53d1420
takken08 <yoshitake130814@gmail.com>
2022-06-25 23:02:15 +0900

    productモデルにデータを保存する記述をした

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,15 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def product_params
