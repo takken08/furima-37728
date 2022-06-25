a1f87cf7ba06c8fe29c7c67bc6dab2527d483db6
takken08 <yoshitake130814@gmail.com>
2022-06-25 23:22:28 +0900

    コードをまとめた

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,11 @@ class Product < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :item_state, :delivery_charge, :area, :delivery_day
+  belongs_to :category
+  belongs_to :item_state
+  belongs_to :delivery_charge
+  belongs_to :area
+  belongs_to :delivery_day
 
   validates :item_name, :explanation, :category_id, :item_state_id, :delivery_charge_id, :area_id, :delivery_day_id, :selling_price, presence: true
   validates :category_id, :item_state_id, :delivery_charge_id, :area_id, :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" }
