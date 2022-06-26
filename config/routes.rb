262a63bae8b87bf1a774eb20b7c5a54f12a235f1
takken08 <yoshitake130814@gmail.com>
2022-06-26 21:44:19 +0900

    商品をdbに登録できるようになった

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
-  resources :products, only: [:index, :new]
+  
+  resources :products, only: [:index, :new, :create]
 end
