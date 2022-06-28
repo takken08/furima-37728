15a273e05eac04c0bb100432ce047521013d24a3
takken08 <yoshitake130814@gmail.com>
2022-06-28 19:01:10 +0900

    エラーハンドリングの表示とバリデーションの追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,9 @@ class Product < ApplicationRecord
   belongs_to :area
   belongs_to :delivery_day
 
-  validates :item_name, :explanation, :category_id, :item_state_id, :delivery_charge_id, :area_id, :delivery_day_id, :selling_price, presence: true
+  Valid_price_num = /\A[0-9]+\z/
+
+  validates :item_name, :explanation, :category_id, :item_state_id, :delivery_charge_id, :area_id, :delivery_day_id, :selling_price, :image, presence: true
+  validates :selling_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "please enter the price from 300 yen to 9,999,999 yen" }, format: { with: Valid_price_num, message: 'please enter in half-width digit' }
   validates :category_id, :item_state_id, :delivery_charge_id, :area_id, :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" }
 end
