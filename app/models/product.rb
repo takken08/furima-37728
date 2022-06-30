3e1fcfe374cfab428cdb583dd029c8cccf79b584
takken08 <yoshitake130814@gmail.com>
2022-06-30 17:29:07 +0900

    selling_priceのバリデーションを編集した

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,11 +9,10 @@ class Product < ApplicationRecord
   belongs_to :area
   belongs_to :delivery_day
 
-  Valid_price_num = /\A[0-9]+\z/
 
   validates :item_name, :explanation, :category_id, :item_state_id, :delivery_charge_id, :area_id, :delivery_day_id, :selling_price, :image, presence: true
   validates :item_name, length: { maximum: 40, message: 'please enter within 40 characters' }
   validates :explanation, length: { maximum: 1000, message: 'please enter within 1000 characters' }
-  validates :selling_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "please enter the price from 300 yen to 9,999,999 yen" }, format: { with: Valid_price_num, message: 'please enter in half-width digit' }
+  validates :selling_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "please enter the price from 300 yen to 9,999,999 yen in half-width digit" }
   validates :category_id, :item_state_id, :delivery_charge_id, :area_id, :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" }
 end
