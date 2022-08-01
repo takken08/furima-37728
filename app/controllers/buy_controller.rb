0d61836cb4df4394ef32fd864243a78a3fc9dc5a
takken08 <yoshitake130814@gmail.com>
2022-08-01 21:51:55 +0900

    buy controllerを作成した

---
+++ app/controllers/buy_controller.rb
@@ -0,0 +1,5 @@
+class BuyController < ApplicationController
+  before_action :authenticate_user!
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "products#index"
   
   resources :products, only: [:index, :new, :create, :destroy, :edit, :update, :show]
+  resources :buy, only: [:index, :new]
 end
