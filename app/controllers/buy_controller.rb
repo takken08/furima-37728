2c16fb10bbbfd907222c0e32c5a1a3f4ef4f5d7f
takken08 <yoshitake130814@gmail.com>
2022-08-03 19:15:57 +0900

    buyコントローラーにインスタンスを記述した

--- app/controllers/buy_controller.rb
+++ app/controllers/buy_controller.rb
@@ -1,5 +1,19 @@
 class BuyController < ApplicationController
   before_action :authenticate_user!
   def index
+    @shopping_address = ShoppingAddress.new
   end
+
+  def create
+    @shopping_address = ShoppingAddress.new(buy_params)
+    if @shopping_address.valid?
+      @shopping_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  def buy_params
+    params.require(:shopping_address).permit(:post_code, :area_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id])
 end
