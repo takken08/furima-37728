1ec3bf24e52442361c4f28757b4cfbf8b04e1143
takken08 <yoshitake130814@gmail.com>
2022-08-04 17:51:21 +0900

    建物名のバリデーションの記述を削除した

--- app/models/shopping_address.rb
+++ app/models/shopping_address.rb
@@ -6,7 +6,6 @@ class ShoppingAddress
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :municipality
     validates :house_number
-    validates :building_name
     validates :phone_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, greater_than_or_equal_to: 10, less_than_or_equal_to: 11, message: "Only half-width alphanumcric characters between 10 and 11 digits can be saved."}
     validates :user_id
     validates :product_id
