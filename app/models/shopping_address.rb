228805200088b9a45cc2f130a79cb537561b097f
takken08 <yoshitake130814@gmail.com>
2022-08-08 18:43:22 +0900

    モデルで扱えるように記述した

--- app/models/shopping_address.rb
+++ app/models/shopping_address.rb
@@ -1,6 +1,6 @@
 class ShoppingAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :area_id, :municipality, :house_number, :building_name, :phone_number, :user_id, :product_id
+  attr_accessor :post_code, :area_id, :municipality, :house_number, :building_name, :phone_number, :user_id, :product_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
