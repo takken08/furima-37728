d3071a5e3eede7c02fb8477462eb9c2e017844ba
takken08 <yoshitake130814@gmail.com>
2022-08-11 22:14:05 +0900

    7つのアクションが定義されているため記述を削除した

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
   
-  resources :products, only: [:index, :new, :create, :destroy, :edit, :update, :show] do
+  resources :products do
     resources :buy, only: [:index, :create]
   end
 end
