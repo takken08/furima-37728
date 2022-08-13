a315ca56026258ec240ede00b75c28c927ba41b4
takken08 <yoshitake130814@gmail.com>
2022-08-13 17:40:32 +0900

    マイグレーションファイルを修正した

--- db/migrate/20220801125529_create_addresses.rb
+++ db/migrate/20220801125529_create_addresses.rb
@@ -3,17 +3,10 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
     create_table :addresses do |t|
       t.string     :post_code,    null: false
       t.integer    :area_id,      null: false
-<<<<<<< HEAD
-      t.string     :municipality
-      t.string     :house_number
-      t.string     :building_name
-      t.string     :phone_number
-=======
       t.string     :municipality, null: false
       t.string     :house_number, null: false
       t.string     :building_name
       t.string     :phone_number, null: false
->>>>>>> parent of d05285f (Revert "マイグレーションを修正した")
       t.references :buy,          null: false, foreign_key: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -36,10 +36,10 @@ ActiveRecord::Schema.define(version: 2022_08_01_125529) do
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_code", null: false
     t.integer "area_id", null: false
-    t.string "municipality"
-    t.string "house_number"
+    t.string "municipality", null: false
+    t.string "house_number", null: false
     t.string "building_name"
-    t.string "phone_number"
+    t.string "phone_number", null: false
     t.bigint "buy_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
