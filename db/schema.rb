06f75ff360a5f19b7e3e2d1f814ace98a093be13
takken08 <yoshitake130814@gmail.com>
2022-08-13 17:23:24 +0900

    Revert "マイグレーションを修正した"
    This reverts commit 67da651ae338442b9d1e16845d1fe35dacc76a37.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_01_125529) do
+ActiveRecord::Schema.define(version: 2022_08_01_125405) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,19 +31,6 @@ ActiveRecord::Schema.define(version: 2022_08_01_125529) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "post_code", null: false
-    t.integer "area_id", null: false
-    t.string "municipality", null: false
-    t.string "house_number", null: false
-    t.string "building_name"
-    t.string "phone_number", null: false
-    t.bigint "buy_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["buy_id"], name: "index_addresses_on_buy_id"
   end
 
   create_table "buys", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -89,7 +76,6 @@ ActiveRecord::Schema.define(version: 2022_08_01_125529) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "addresses", "buys"
   add_foreign_key "buys", "products"
   add_foreign_key "buys", "users"
   add_foreign_key "products", "users"
