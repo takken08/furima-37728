d279cbfbf3441659f799fef1e786d7f0ce43a12d
takken08 <yoshitake130814@gmail.com>
2022-08-13 17:42:23 +0900

    建物名の定義をした

--- spec/factories/shopping_addresses.rb
+++ spec/factories/shopping_addresses.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     area_id      {Faker::Number.between(from: 2, to: 48)}
     municipality {Faker::Address.city}
     house_number { '青山1-1-1' }
+    building_name ( '春日部' )
     phone_number { '09012345678' }
     token { "tok_abcdefghijk00000000000000000" }
   end
